d5473604371c2b6791aea2bb964933583710eb5c
miguel18100 <miguelmunoz181007@gmail.com>
2026-09-19 14:48:54 -0500

    Delete sena-asistencias directory

--- sena-asistencias/fix-login.sql
+++
@@ -1,3 +0,0 @@
--- Ejecuta este archivo una sola vez en phpMyAdmin si ya habías importado database.sql.
-USE sena_asistencias;
-UPDATE admins SET password_hash = 'TEMP:control1234' WHERE usuario = 'admin';
--- sena-asistencias/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/config.php';
-
-session_destroy();
-
-header('Location: index.php');
-
-?>
